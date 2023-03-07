fc682ebe2708eb35295de91ca6b4f50e9c2e2790
Blacky <64606171+Blaackyy@users.noreply.github.com>
2023-03-07 12:28:31 -0300

    Initial commit

---
+++ src/main/java/dev/blacky/housing/Housing.java
@@ -0,0 +1,17 @@
+package dev.blacky.housing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Housing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
